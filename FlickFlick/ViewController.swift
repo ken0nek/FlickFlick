f52c9409fda4e936401ec1e4a8929e7b585975e1
ken0nek <ken.for.develop@gmail.com>
2015-07-22 22:53:54 +0900

    Initial Commit

---
+++ FlickFlick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickFlick
+//
+//  Created by Ken Tominaga on 7/22/15.
+//  Copyright (c) 2015 ken0nek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
