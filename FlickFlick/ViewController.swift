5db2b22c33efaeca12fddb2b7fdfc2b4db0b2516
ken0nek <ken.for.develop@gmail.com>
2015-08-14 21:35:27 +0900

    Make code simple

--- FlickFlick/ViewController.swift
+++ FlickFlick/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     @IBOutlet private weak var arrowsLabel: UILabel!
     
     // key : UISwipeGestureRecognizerDirection.rawValue
-//    private let dic: [UInt: String] = [1: "→", 2: "←", 4: "↑", 8: "↓"]
+    //    private let dic: [UInt: String] = [1: "→", 2: "←", 4: "↑", 8: "↓"]
     // key : UISwipeGestureRecognizerDirection
     private let dic2: [UISwipeGestureRecognizerDirection: String] = [.Right: "→", .Left: "←", .Up: "↑", .Down: "↓"]
     
@@ -28,13 +28,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    
-//        for i in [UInt](dic.keys) {
-//            let swipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "didSwipe:")
-//            let direction = UISwipeGestureRecognizerDirection(i)
-//            swipeGestureRecognizer.direction = direction
-//            view.addGestureRecognizer(swipeGestureRecognizer)
-//        }
+        
+        //        for i in [UInt](dic.keys) {
+        //            let swipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "didSwipe:")
+        //            let direction = UISwipeGestureRecognizerDirection(i)
+        //            swipeGestureRecognizer.direction = direction
+        //            view.addGestureRecognizer(swipeGestureRecognizer)
+        //        }
         
         for dir in [UISwipeGestureRecognizerDirection](dic2.keys) {
             let swipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "didSwipe:")
@@ -44,17 +44,17 @@ class ViewController: UIViewController {
         
         newGame()
     }
-
+    
     func didSwipe(sender: UISwipeGestureRecognizer) { // private
-//        if dic[sender.direction.rawValue] == directions.first! {
-//            directions.removeAtIndex(0)
-//            arrowsLabel.text = makeSimpleStringFromArray(directions)
-////            arrowsLabel.attributedText = makeHighlightedStringFromArray(directions)
-//        }
+        //        if dic[sender.direction.rawValue] == directions.first! {
+        //            directions.removeAtIndex(0)
+        //            arrowsLabel.text = makeSimpleStringFromArray(directions)
+        ////            arrowsLabel.attributedText = makeHighlightedStringFromArray(directions)
+        //        }
         if dic2[sender.direction] == directions.first! {
             directions.removeAtIndex(0)
-            arrowsLabel.text = makeSimpleStringFromArray(directions)
-//            arrowsLabel.attributedText = makeHighlightedStringFromArray(directions)
+            //            arrowsLabel.text = makeSimpleStringFromArray(directions)
+            arrowsLabel.attributedText = makeHighlightedStringFromArray(directions)
         }
         
         if directions.isEmpty {
@@ -63,20 +63,20 @@ class ViewController: UIViewController {
     }
     
     private func produceArrayCombination() -> [String] {
-//        var dirs = [String]()
-//        let arrows = [String](dic.values)
-//        for _ in 0 ..< 4 {
-//            dirs.append(arrows[Int(arc4random_uniform(UInt32(4)))])
-//        }
-//        return dirs
+        //        var dirs = [String]()
+        //        let arrows = [String](dic.values)
+        //        for _ in 0 ..< 4 {
+        //            dirs.append(arrows[Int(arc4random_uniform(UInt32(4)))])
+        //        }
+        //        return dirs
         let arrows = [String](dic2.values)
         return Array(0..<4).map{_ in arrows[Int(arc4random_uniform(UInt32(4)))]}
-//        var dirs = [String]()
-//        let arrows = [String](dic2.values)
-//        for _ in 0 ..< 4 {
-//            dirs.append(arrows[Int(arc4random_uniform(UInt32(4)))])
-//        }
-//        return dirs
+        //        var dirs = [String]()
+        //        let arrows = [String](dic2.values)
+        //        for _ in 0 ..< 4 {
+        //            dirs.append(arrows[Int(arc4random_uniform(UInt32(4)))])
+        //        }
+        //        return dirs
     }
     
     // ["→", "←", "↑", "↑"] -> "→←↑↑"
@@ -86,34 +86,26 @@ class ViewController: UIViewController {
     
     // show combination and highlight the target(first) arrow
     private func makeHighlightedStringFromArray(array: [String]) -> NSMutableAttributedString {
-        var labelText = NSMutableAttributedString()
-        
-        for i in 0..<array.count {
-            
-            let attributedString: NSAttributedString
-            if i == 0 {
-                attributedString = NSAttributedString(string: "\(array[i])", attributes: [NSForegroundColorAttributeName: UIColor.redColor()])
-            } else {
-                attributedString = NSAttributedString(string: "\(array[i])")
-            }
-            
-            labelText.appendAttributedString(attributedString)
+        if array.isEmpty {
+            return NSMutableAttributedString(string: "")
         }
         
-        return labelText
+        let attributedString = NSMutableAttributedString(string: "".join(array.map{"\($0)"}))
+        attributedString.addAttribute(NSForegroundColorAttributeName, value: UIColor.redColor(), range: NSMakeRange(0, 1))
+        return attributedString
     }
     
     private func newGame() {
         directions = produceArrayCombination()
-        arrowsLabel.text = makeSimpleStringFromArray(directions)
-//        arrowsLabel.attributedText = makeHighlightedStringFromArray(directions)
+        //        arrowsLabel.text = makeSimpleStringFromArray(directions)
+        arrowsLabel.attributedText = makeHighlightedStringFromArray(directions)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
